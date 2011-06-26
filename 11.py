7e2af8b0c6261a74f74c406402ac24ef4ffa6d85
isnowfy <isnowfy@gmail.com>
2011-06-26 22:47:32 +0800

    12level again

--- 11.py
+++ 11.py
@@ -7,13 +7,9 @@ even=Image.new("RGB",(w/2,h/2))
 for x in range(0,w):
 	for y in range(0,h):
 		pixel = img.getpixel((x, y))  
-		if x % 2 == 0 and y % 2 == 0:  
-			odd.putpixel((x/2, y/2), pixel)  
-		elif x % 2 == 1 and y % 2 == 0:  
-			even.putpixel(((x-1)/2, y/2), pixel)  
-		elif x % 2 == 0 and y % 2 == 1:  
-			even.putpixel((x/2, (y-1)/2), pixel)  
-		elif x % 2 == 1 and y % 2 == 1:  
-			odd.putpixel(((x-1)/2, (y-1)/2), pixel)  
+		if (x%2)^(y%2)==0:  
+			odd.putpixel((x/2,y/2), pixel)  
+		else:  
+			even.putpixel((x/2,y/2), pixel) 
 odd.show()  
 even.show()  
